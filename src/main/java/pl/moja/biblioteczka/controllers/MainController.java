8ab69ff9a932c1820e77fd853106c50ea3760517
Jakub Zaboronek <jakubzaboronek@wp.pl>
2018-10-16 05:32:22 +0200

    Include menu view in main view

--- src/main/java/pl/moja/biblioteczka/controllers/MainController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/MainController.java
@@ -1,4 +1,19 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
 public class MainController {
+
+    @FXML
+    private TopMenuButtonsController topMenuButtonsController;
+    @FXML
+    private BorderPane borderPane;
+
+    private void initialize() {
+        topMenuButtonsController.setMainController(this);
+
+    }
+
+
 }
--- src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
+++ src/main/java/pl/moja/biblioteczka/controllers/TopMenuButtonsController.java
@@ -1,4 +1,28 @@
 package pl.moja.biblioteczka.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class TopMenuButtonsController {
+
+    private MainController mainController;
+
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    @FXML
+    public void openLibrary() {
+        System.out.println("Open libraty");
+    }
+    @FXML
+    public void openListBooks() {
+        System.out.println("Open list books");
+    }
+    @FXML
+    public void openStatistic() {
+        System.out.println("Open statustsaic");
+    }
+
+
 }
