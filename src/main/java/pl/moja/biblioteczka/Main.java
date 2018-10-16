7df8677a56290e0265051445b6035040f25f2d19
Jakub Zaboronek <jakubzaboronek@wp.pl>
2018-10-16 05:55:51 +0200

    Added Resource Bundle(en and pl)

--- src/main/java/pl/moja/biblioteczka/Main.java
+++ src/main/java/pl/moja/biblioteczka/Main.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -13,11 +16,14 @@ public class Main extends Application {
     }
 
     public void start(Stage primarystage) throws Exception {
+//        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primarystage.setScene(scene);
-        primarystage.setTitle("Moja Biblioteczka");
+        primarystage.setTitle(bundle.getString("title.application"));
         primarystage.show();
 
     }
